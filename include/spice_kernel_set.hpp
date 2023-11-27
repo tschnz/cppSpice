2264645806a47b02dc96ae8ee757a98e676cd0bc
Jens Schindel <jens@resolume.com>
2023-11-27 11:58:05 +0100

    Include limits header

--- include/spice_kernel_set.hpp
+++ include/spice_kernel_set.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <map>
 #include <string>
+#include <limits>
 
 namespace Spice
 {    
--- include/spice_label_map.hpp
+++ include/spice_label_map.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <limits>
 
 namespace Spice
 {
